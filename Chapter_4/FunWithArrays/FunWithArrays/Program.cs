f648246aed57b637ff167f2f75781529568ca3c9
Eugen <zenyakolb@gmail.com>
2023-08-11 21:32:29 +0300

    commit in SimpleArrays first

---
+++ Chapter_4/FunWithArrays/FunWithArrays/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Arrays *****\n");
+            SimpleArrays();
+        }
+
+        #region Simple Array
+        static void SimpleArrays()
+        {
+            Console.WriteLine("=> Simple Array Creation.");
+            // Create and fill an array of 3 Integers
+            int[] myInts = new int[3];
+            myInts[0] = 100;
+            myInts[1] = 200;
+            myInts[2] = 300;
+
+            // Now print each value.
+            foreach (int i in myInts)
+                Console.WriteLine(i);
+            Console.WriteLine();
+        }
+        #endregion
+    }
+}
